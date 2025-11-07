4f4e5630fc294564af0b8fd57eab2f6bf10331fb
rosoefox <foxdragonscent@gmail.com>
2025-11-07 14:15:58 +0900

    E05 변경

--- src/components/example3/ChildComponent.vue
+++ src/components/example3/ChildComponent.vue
@@ -1,13 +1,19 @@
-<!-- ChildComponent.vue -->
 <template>
   <div>
     <p>{{ message }}</p>
-    <button @click="$emit('custom-event', 'Hello from child')">Send Event</button>
+    <button @click="emit('custom-event', 'Hello from child')">Send Event</button>
   </div>
 </template>
 
-<script>
-export default {
-  props: ['message']
-};
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  message: {
+    type: String,
+    required: true
+  }
+});
+
+const emit = defineEmits(['custom-event']);
 </script>
