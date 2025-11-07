232f7e2e91b3148e533b3f88443e1bf6aa8a7dab
rosoefox <foxdragonscent@gmail.com>
2025-11-07 14:32:06 +0900

    E06 변경

--- src/components/example4/ParentComponent.vue
+++ src/components/example4/ParentComponent.vue
@@ -1,23 +1,13 @@
-<!-- ParentComponent.vue -->
 <template>
   <div>
     <ChildComponent1 />
   </div>
 </template>
 
-<script>
-
+<script setup>
+import { provide, ref } from 'vue';
 import ChildComponent1 from "@/components/example4/ChildComponent1.vue";
 
-export default {
-  name: "E06ParentComponent",
-  provide() {
-    return {
-      sharedMessage: 'Hello from provide'
-    };
-  },
-  components: {
-    ChildComponent1
-  }
-};
+const sharedMessage = ref('Hello from provide');
+provide('sharedMessage', sharedMessage);
 </script>
